24360e4cdac93f4b6f2ca2f1cdfe4078d048eb11
Sakthi <120540326+sakthizxc@users.noreply.github.com>
2023-01-24 22:25:52 +0530

    Create Theater.java

---
+++ Theater.java
@@ -0,0 +1,23 @@
+class Theater{
+int total;
+public static void main(String []args)
+{
+Theater raja=new Theater(200);
+System.out.println("After booking I have");
+raja.bookTicket();
+}
+Theater(int amount)
+{
+total=amount;
+}
+void bookTicket()
+{
+int Ticket_price=120;
+int ballence=total-Ticket_price;
+System.out.println(ballence+" "+"Ballence");
+}
+}
+
+My Output is:
+After booking I have
+80 Ballence
