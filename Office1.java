ee317c9255e49072a913299bea7e76304ff7b4fb
sakthizxc <sakthivel8sakthi@gmail.com>
2023-01-25 15:45:15 +0530

    First program

---
+++ Office1.java
@@ -0,0 +1,26 @@
+package tamilnadu.chennai;
+class Office1
+{
+static String name ="aswin";
+public static void main(String[] args)
+{
+Office1 manager= new Office1();
+manager.work();
+manager.makeprofit();
+}
+void work()
+{
+System.out.println("office at work");
+}
+private void makeprofit()
+{
+System.out.println("make profit");
+}
+}
+
+
+
+
+
+
+
